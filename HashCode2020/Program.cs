8be71850c894df25c10d60e678a643f58fce435f
Mathias Feitzinger <dev@feitzinger.eu>
2020-02-20 17:15:05 +0100

    Init

---
+++ HashCode2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
