f30f92b5f4995f1c3e0818c5c4312fc5decf8507
Mathias Feitzinger <dev@feitzinger.eu>
2020-02-20 20:43:17 +0100

    write output files

--- FileImporter/Library.cs
+++ FileImporter/Library.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using MoreLinq;
 
 namespace FileImporter
 {
@@ -11,41 +12,49 @@ namespace FileImporter
         {
             LibraryId = libraryId;
             var firstLineValues = firstLine.Split(" ");
-            BooksInLibrary = int.Parse(firstLineValues[0]);
+            var booksInLibrary = int.Parse(firstLineValues[0]);
             SetupTime = int.Parse(firstLineValues[1]);
             BooksPerDay = int.Parse(firstLineValues[2]);
 
             var secondLineValues = secondLine.Split(" ");
-            BookIdsInLibrary = secondLineValues.Select(x => allBooks[int.Parse(x)]).ToImmutableDictionary(x => x.Id);
+            BookIdsInLibrary = secondLineValues.Select(x => allBooks[int.Parse(x)]).ToDictionary(x => x.Id);
             BooksOrderedByScore =
                 BookIdsInLibrary.Select(x => x.Value).OrderByDescending(x => x.Score).ToImmutableList();
         }
 
         public int LibraryId { get; }
 
-        public int BooksInLibrary { get; }
         public int SetupTime { get; }
         public int BooksPerDay { get; }
-        public ImmutableDictionary<int, Book> BookIdsInLibrary { get; }
+        public Dictionary<int, Book> BookIdsInLibrary { get; private set; }
 
         public ImmutableList<Book> BooksOrderedByScore { get; }
 
-        public int MaxPointsForPeriod(int availableDays, IDictionary<int, Book> books)
+
+        public void MarkBooksAsProcessedByOtherLibrary(List<Book> processedBooks)
+        {
+            processedBooks.ForEach(x => BookIdsInLibrary.Remove(x.Id));
+        }
+
+        public KeyValuePair<int, List<Book>> MaxPointsForPeriod(int availableDays, IDictionary<int, Book> books)
         {
             var workingPeriod = availableDays - SetupTime;
-            if (workingPeriod <= 0) return 0;
+            if (workingPeriod <= 0) return new KeyValuePair<int, List<Book>>(0, new List<Book>());
 
             var possibleScore = 0;
             var maxPossibleBooksToProcess = workingPeriod * BooksPerDay;
             maxPossibleBooksToProcess = maxPossibleBooksToProcess > BooksOrderedByScore.Count()
                 ? BooksOrderedByScore.Count()
                 : maxPossibleBooksToProcess;
+
+            var booksWhichAreProcessed = new List<Book>();
             for (int i = 0; i < maxPossibleBooksToProcess; i++)
             {
-                possibleScore += books[i].Score;
+                var book = books[i];
+                possibleScore += book.Score;
+                booksWhichAreProcessed.Add(book);
             }
-
-            return possibleScore;
+            return new KeyValuePair<int, List<Book>>(possibleScore, booksWhichAreProcessed);
         }
     }
 }
--- HashCode2020/Program.cs
+++ HashCode2020/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using FileImporter;
 using Serilog;
 
 namespace HashCode2020
 {
+    public class LibraryInternSolution
+    {
+        public Library libary { get; }
+        public KeyValuePair<int, List<Book>> KeyValue { get; }
+
+        public LibraryInternSolution(Library libary, KeyValuePair<int, List<Book>> keyValue)
+        {
+            this.libary = libary;
+            KeyValue = keyValue;
+        }
+    }
+
     public class Program
     {
         private static void Main(string[] args)
@@ -19,22 +32,39 @@ namespace HashCode2020
             // Log.Information("Input is {@First}", inputFile);
 
             var availableDays = libraryProblem.Days;
-            var librarySolution = new List<Library>();
+            var librarySolution = new List<LibraryInternSolution>();
             var availableLibrary = libraryProblem.Libraries.Select(x => x.Value).ToList();
             while (availableDays > 0)
             {
                 var nextLibrary = availableLibrary
-                    .OrderByDescending(x => x.MaxPointsForPeriod(availableDays, libraryProblem.Books)).FirstOrDefault();
-                if (nextLibrary == null)
+                    .Select(x =>
+                        new LibraryInternSolution(x, x.MaxPointsForPeriod(availableDays, libraryProblem.Books)))
+                    .OrderByDescending(x => x.KeyValue.Key)
+                    .FirstOrDefault();
+                if (nextLibrary?.libary == null)
                 {
                     break;
                 }
-                librarySolution.Add(nextLibrary);
-                availableLibrary.Remove(nextLibrary);
-            }
-            
-            
 
+                librarySolution.Add(nextLibrary);
+                availableLibrary.Remove(nextLibrary.libary);
+                availableLibrary.ForEach(x => x.MarkBooksAsProcessedByOtherLibrary(nextLibrary.KeyValue.Value));
+            }
+
+            WriteOutputFile(librarySolution);
+        }
+
+        public static void WriteOutputFile(List<LibraryInternSolution> librarySolution)
+        {
+            using var streamWriter = File.CreateText("output.txt");
+            streamWriter.WriteLine(librarySolution.Count);
+
+
+            librarySolution.ForEach(library =>
+            {
+                streamWriter.WriteLine($"{library.libary.LibraryId} {library.KeyValue.Value.Count}");
+                streamWriter.WriteLine(string.Join(" ", library.KeyValue.Value.Select(x => x.Id)));
+            });
         }
     }
 }
